c94ed3206d17fc579208de0fc3d0dcdd89d579aa
naldo <jhpjhp0223@gmail.com>
2014-02-25 01:16:30 +0900

    [Layout] Basic

--- kfac/kfac/urls.py
+++ kfac/kfac/urls.py
@@ -4,4 +4,5 @@ from django.http import *
 urlpatterns = patterns('',
     (ur'^$', lambda request: HttpResponseRedirect('/main/')),
     (ur'^account/', include('account.urls')),
+    (ur'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': './media'}),
 )
