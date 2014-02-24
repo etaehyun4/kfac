ba5d91d3b20ed40660b2161caf323095eb194163
naldo <jhpjhp0223@gmail.com>
2014-02-25 01:26:36 +0900

    Fix bug in urls.py

--- kfac/kfac/urls.py
+++ kfac/kfac/urls.py
@@ -5,9 +5,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     (ur'^$', lambda request: HttpResponseRedirect('/main/')),
     (ur'^account/', include('account.urls')),
-<<<<<<< HEAD
     (ur'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': './media'}),
-=======
     (ur'^admin/', include(admin.site.urls)),
->>>>>>> 0e7e5c00cc91818c207e471baa6a859a7fcb178d
 )
