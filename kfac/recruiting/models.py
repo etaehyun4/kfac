fec529fe94009e74cdf0fc8b011b580b4441acf0
whitegold <whitegold@sparcs.org>
2014-02-27 02:55:48 +0900

    [Recruiting] Finish Process Page and make model for "recruiting"

--- kfac/recruiting/models.py
+++ kfac/recruiting/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Application(models.Model):
+    doc = models.FileField(upload_to = "recruiting/app/", default = '')
+
+class Guideline(models.Model):
+    doc = models.FileField(upload_to = "recruiting/guide/", default = '')
+
+class Question(models.Model):
+    question = models.CharField(max_length = 100)
+    writer = models.ForeignKey(User)
+    date = models.DateField(auto_now = True)
+    click = models.IntegerField(default = 0)
+    answer = models.CharField(max_length = 500)
+
+class AppAdmin(admin.ModelAdmin):
+    list_display = ('doc',)
+
+class GuideAdmin(admin.ModelAdmin):
+    list_display = ('doc',)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question', 'writer', 'date', 'answer')
+
+admin.site.register(Application, AppAdmin)
+admin.site.register(Guideline, GuideAdmin)
+admin.site.register(Question, QuestionAdmin)
--- kfac/recruiting/views.py
+++ kfac/recruiting/views.py
@@ -4,6 +4,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext, Context
 from django import template
+from models import *
 
 template.add_to_builtins('django.templatetags.i18n')
 
@@ -14,9 +15,31 @@ def require(request):
     }, context_instance=RequestContext(request))
 
 def process(request):
+    app = Application.objects.all().order_by('-id')
+    guide = Guideline.objects.all().order_by('-id')
+    
+    app_result = True
+    guide_result = True
+    app_url = ""
+    guide_url = ""
+
+    if len(app) == 0:
+        app_result = False
+    else:
+        app_url = app[0].doc.url
+
+    if len(guide) == 0:
+        guide_result = False
+    else:
+        guide_url = guide[0].doc.url
+
     return render_to_response('recruiting/process.html',{
         'menu':'recruiting',
         'submenu':'process',
+        'app_result':app_result,
+        'guide_result':guide_result,
+        'app_url': app_url,
+        'guide_url': guide_url,
     }, context_instance=RequestContext(request))
 
 def faq(request):
