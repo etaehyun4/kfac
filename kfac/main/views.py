7e8faace5b7c3ac91935a51f8b421d069fa209d8
aon <jwchae8@gmail.com>
2014-03-17 08:49:18 +0900

    [About & Main] Gradation applied & Recent articles are shown in the main page

--- kfac/main/views.py
+++ kfac/main/views.py
@@ -4,8 +4,17 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.template import RequestContext, Context
+from only.models import Board, Article
 
 def home(request):
+    b = Board.objects.get(name='board')
+    a = Article.objects.all().order_by('-id').filter(board=b)
+    if a.count() >= 4:
+        ret = a[0:4]
+    else:
+        ret = a
     return render_to_response('main/main.html',{
         'menu':'main',
+        'articles':ret,
+        'board':b,
     }, context_instance=RequestContext(request))
