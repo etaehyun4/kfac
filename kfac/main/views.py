b8e9a01903d72e9b9f94d51deaf7bb5d9875adc6
aon <jwchae8@gmail.com>
2014-03-17 17:16:15 +0900

    [Main] complete main

--- kfac/main/views.py
+++ kfac/main/views.py
@@ -4,15 +4,18 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.template import RequestContext, Context
-from only.models import Board, Article
+from only.models import Board, Article, Comment
 
 def home(request):
     b = Board.objects.get(name='board')
     a = Article.objects.all().order_by('-id').filter(board=b)
+    ret = []
     if a.count() >= 4:
-        ret = a[0:4]
+        for i in range(4):
+            ret.append((a[i],Comment.objects.all().filter(article=a[i]).count()))
     else:
-        ret = a
+        for i in range(a.count()):
+            ret.append((a[i],Comment.objects.all().filter(article=a[i]).count()))
     return render_to_response('main/main.html',{
         'menu':'main',
         'articles':ret,
